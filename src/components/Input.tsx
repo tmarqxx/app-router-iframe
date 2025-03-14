e14ff07600acdbb42291cc5a997d1feca4790fdb
tmarqxx <tgmarquise@gmail.com>
2025-03-13 17:23:34 -0700

    chore: upgrade to tailwind v4

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
-      className="mx-1 h-10 appearance-none rounded-md border border-slate-400/25 bg-slate-300/35 p-2 shadow-sm shadow-cyan-700/35 outline-blue-600"
+      className="mx-1 h-10 appearance-none rounded-md border border-slate-400/25 bg-slate-300/35 p-2 shadow-xs shadow-cyan-700/35 outline-blue-600"
     >
       {props.children}
     </input>
